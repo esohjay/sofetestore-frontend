e845f395f330f4ae758e068be14544e5087f350d
unknown <esohjay3@gmail.com>
2021-10-02 10:06:47 +0100

    removed border on all clickable components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 //import "./index.css";
 import "./style.css";
+import "focus-visible/dist/focus-visible";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-
+import { Global, css } from "@emotion/react";
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <ChakraProvider>
+        <Global styles={GlobalStyles} />
         <App />
       </ChakraProvider>
     </React.StrictMode>
